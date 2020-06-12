d34976bf705cbea5846901343eb71eef696a36c1
MartinG94 <guillenmartin94@gmail.com>
2020-06-12 17:34:27 -0300

    Sistema de roteo para movies y actors

--- site/app.js
+++ site/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
+var actorsRouter = require('./routes/actors');
 
 var app = express();
 
@@ -20,6 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/movies', moviesRouter);
+app.use('/actors', actorsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
