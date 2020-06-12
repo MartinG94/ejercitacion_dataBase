b0b2642ae7a53661f3b092d706e87472ec45ddff
MartinG94 <guillenmartin94@gmail.com>
2020-06-12 18:57:47 -0300

    TO DO

--- site/routes/movies.js
+++ site/routes/movies.js
@@ -5,4 +5,10 @@ var moviesController = require('../controllers/moviesController');
 /* GET home page. */
 router.get('/', moviesController.listar);
 router.get('/detail/:id', moviesController.getById);
+
+/* TO DO
+router.put('/detail/:id', moviesController.actualizarPelicula);
+router.delete('/detail/:id', moviesController.eliminarPelicula);
+*/
+
 module.exports = router;
