83076dad5e70575c0a8060d9f8beeb532ed5571d
MartinG94 <guillenmartin94@gmail.com>
2020-06-12 17:50:55 -0300

    Se requiere el controlador correspondiente

--- site/routes/movies.js
+++ site/routes/movies.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var moviesController = require('../controllers/moviesController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', moviesController.listar);
 
 module.exports = router;
