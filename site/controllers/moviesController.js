ee1981f28ce6c050b2bb1302e2264bda32a7bc6d
MartinG94 <guillenmartin94@gmail.com>
2020-06-13 02:45:18 -0300

    Return

--- site/controllers/moviesController.js
+++ site/controllers/moviesController.js
@@ -6,39 +6,39 @@ const controller = {
         db.Peliculas.findAll()
             .then( (resultado) => {
                 let peliculas = resultado;
-                res.render('movies', {
+                return res.render('movies', {
                     title: 'Peliculas',
                     peliculas: peliculas
                 });
             })
             .catch( (error) => {
-                res.send('Ocurrió un error');
+                return res.send('Ocurrió un error');
             });
     },
     getById: (req, res) => {
         const idPedido = req.params.id;
         db.Peliculas.findByPk(idPedido)
             .then( (pelicula) => {
-                res.render('movieDetail', {
+                return res.render('movieDetail', {
                     title: 'Detail',
                     pelicula: pelicula
                 });
             })
             .catch( (error) => {
-                res.send('Ocurrió un error');
+                return res.send('Ocurrió un error');
             });
     },
     getByIdForEdit: (req, res) => {
         const idPedido = req.params.id;
         db.Peliculas.findByPk(idPedido)
             .then( (pelicula) => {
-                res.render('movieEdit', {
+                return res.render('movieEdit', {
                     title: 'Detail',
                     pelicula: pelicula
                 });
             })
             .catch( (error) => {
-                res.send('Ocurrió un error');
+                return res.send('Ocurrió un error');
             });
     },
     actualizarPelicula: (req, res) => {
@@ -50,35 +50,35 @@ const controller = {
             awards: body.awards,
             length: body.length
         }).then( () => {
-            res.redirect(`/movies/detail/${idPelicula}`);
+            return res.redirect(`/movies/detail/${idPelicula}`);
         }).catch( (error) => {
-            res.send('Ocurrió un error');
+            return res.send('Ocurrió un error');
         });
     },
     confirmarEliminacion: (req, res) => {
         const idPelicula = req.params.id;
         db.Peliculas.findByPk(idPelicula)
             .then( (pelicula) => {
-                res.render('deleteFormMovie', {
+                return res.render('deleteFormMovie', {
                     title: 'Eliminar Pelicula',
                     pelicula: pelicula
                 })
             })
             .catch( (error) => {
-                res.send('Ocurrió un error');
+                return res.send('Ocurrió un error');
             });
     },
     eliminarPelicula: (req, res) => {
         const idPelicula = req.params.id;
         db.Peliculas.findByPk(idPelicula)
             .then( (pelicula) => {
-                pelicula.destroy();
+                return pelicula.destroy();
             })
             .then( () => {
-                res.send('Pelicula eliminada de la Base de Datos');
+                return res.send('Pelicula eliminada de la Base de Datos');
             })
             .catch( (error) => {
-                res.send('Ocurrió un error');
+                return res.send('Ocurrió un error');
             });
     },
     lasMasNuevas: (req, res) => {
@@ -88,12 +88,12 @@ const controller = {
             ],
             limit: 5
         }).then( (peliculas) => {
-            res.render('moviesNew', {
+            return res.render('moviesNew', {
                 title: 'Peliculas Recientes',
                 peliculas: peliculas
             });
         }).catch( (error) => {
-            res.send('Ocurrió un error');
+            return res.send('Ocurrió un error');
         });
     },
     recomendadas: (req, res) => {
@@ -102,16 +102,16 @@ const controller = {
                 awards: {[db.Sequelize.Op.gte]: 5}
             }
         }).then( (peliculas) => {
-            res.render('moviesRecommended', {
+            return res.render('moviesRecommended', {
                 title: 'Recomendadas',
                 peliculas: peliculas
             });
         }).catch( (error) => {
-            res.send('Ocurrió un error');
+            return res.send('Ocurrió un error');
         });
     },
     formularioBusqueda: (req, res) => {
-        res.render('searchMovies', {
+        return res.render('searchMovies', {
             title: 'Buscador'
         });
     },
@@ -125,12 +125,12 @@ const controller = {
             if(peliculas.length == 0) {
                 return res.send('No se encontraron resultados para su busqueda');
             };
-            res.render('resultSearchMovies',{
+            return res.render('resultSearchMovies',{
                 title: 'Peliculas Encontradas',
                 peliculas: peliculas
             });
         }).catch( (error) => {
-            res.send('Ocurrió un error');
+            return res.send('Ocurrió un error');
         });
     }
 };
