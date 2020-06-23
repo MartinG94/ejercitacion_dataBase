4474258e93986c5e231f12d725915d4fe06fbbfe
MartinG94 <guillenmartin94@gmail.com>
2020-06-23 18:04:38 -0300

    Ruta para acceder al formulario de edicion del actor

--- site/routes/actors.js
+++ site/routes/actors.js
@@ -7,4 +7,7 @@ router.get('/', actorsController.listar);
 
 router.get('/detail/:id', actorsController.getById);
 
+router.get('/edit/:id', actorsController.getByIdForEdit);
+// router.post('/edit/:id', actorsController.actualizarActor);
+
 module.exports = router;
