1a99e124602160046feb861a4e220d712fa0a137
MartinG94 <guillenmartin94@gmail.com>
2020-06-23 17:49:26 -0300

    Método para obtener a un actor mediante su id

--- site/controllers/actorsController.js
+++ site/controllers/actorsController.js
@@ -14,7 +14,24 @@ const controller = {
             .catch( (error) => {
                 return res.send('Ocurrió un error');
             });
-    }
+    },
+    getById: (req, res) => {
+        const idPedido = req.params.id;
+        db.Actor.findByPk(idPedido)
+            .then( (actor) => {
+                if(!actor){
+                    return res.send(`No se encontró una actor con el id: ${idPedido}`);
+                };
+                return res.render('actorDetail', {
+                    title: 'Detail',
+                    actor: actor
+                });
+            })
+            .catch( (error) => {
+                return res.send('Ocurrió un error');
+            })
+    },
+
 };
 
 module.exports = controller;
