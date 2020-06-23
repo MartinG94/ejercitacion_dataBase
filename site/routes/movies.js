15a36da4df69b020d597ac318e6b73a5e569fec3
MartinG94 <guillenmartin94@gmail.com>
2020-06-23 17:30:34 -0300

    Se elimina comentario

--- site/routes/movies.js
+++ site/routes/movies.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var moviesController = require('../controllers/moviesController');
 
-/* GET home page. */
 router.get('/', moviesController.listar);
 
 router.get('/detail/:id', moviesController.getById);
