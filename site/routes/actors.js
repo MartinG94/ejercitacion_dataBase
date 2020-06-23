9447ef7a92f2f0ad0931b75d30828714c12a59dc
MartinG94 <guillenmartin94@gmail.com>
2020-06-23 17:41:09 -0300

    Get para la raíz

--- site/routes/actors.js
+++ site/routes/actors.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var actorsController = require('../controllers/actorsController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', actorsController.listar);
 
 module.exports = router;
